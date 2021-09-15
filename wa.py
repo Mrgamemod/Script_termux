a8218fe4912057a88cdd873caa40e1d8a53edc2a
Mrgamemod <89837275+Mrgamemod@users.noreply.github.com>
2021-09-15 11:36:29 +0700

    Add files via upload

---
+++ wa.py
@@ -0,0 +1,19 @@
+from types import resolve_bases
+from typing import BinaryIO
+
+
+staticmethod{setattr}
+@#mesispdc
+divmod BinaryIO "0000001010110101001010101010101000000000000000000000010
+azcjs8vndfv
+ 
+ df \f'
+ fdf df FileExistsError 
+ e e 
+ d e vdf wefcd
+
+KeyError=resolve_basesrbb\
+
+    rbrb/]r/bb,9rjborbrubirbjrnbiun
+ IsADirectoryErrorDF DF FileExistsErrorV/FVEDFE
+ df varsvf"
